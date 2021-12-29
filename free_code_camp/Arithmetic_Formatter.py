aec338aee638dc4b045ca853cbd0abdcda78f2ec
Amine <96693958+amine-harrane@users.noreply.github.com>
2021-12-29 06:43:59 +0100

    Update Arithmetic_Formatter.py

--- free_code_camp/Arithmetic_Formatter.py
+++ free_code_camp/Arithmetic_Formatter.py
@@ -1,90 +1,45 @@
-# def arithmetic_arranger(list,answer=False):
-#     operator = None
-#     line1 = []
-#     line2 = []
-#     line3 = []
-#     if len(list) > 5 :
-#         return "Error: Too many problems"
-#     for problem in list :
-#         digit = problem.split()
-#         num1 = digit[0]
-#         operator = digit[1] + ' '
-#         num2 = digit[2]
-#         line1.append(num1.rjust(5)+'    ')
-#         line2.append(operator+num2.rjust(3)+'    ')
-#         dash = '-' * max(len(num1),len(num2)) + (2*'-')+'    '
-#         line3.append(dash)
-#     if operator != '+ ' and operator!='- ':
-#         return "Error Operator must be '+' or '-'."
-#     try:
-#         num1=int(num1)
-#         num2=int(num2)
-#     except:
-#         return 'Error: Numbers must only contain digits.'
-#
-#     if num1 // (10**3) > 1:
-#         return 'Error: Numbers cannot be more than four digits.'
-#     if answer==True:
-#         return num1 + num2
-#     print(''.join(line1))
-#     print(''.join(line2))
-#     return ''.join(line3)
-#
-# list = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-# print(arithmetic_arranger(list))
 
-def arithmetic_arranger(list,answer=False):
-    if len(list)>5 :
-        return "Error: Too many problems."
-    line1 = []
-    line2 = []
-    line3 = []
-    line4 = []
-    dash=''
-    for problem in list :
+def arithmetic_arranger(arranger,answer=False):
+    nums1=[]
+    operators_nums2=[]
+    dash = []
+    sums=[]
+    if len(arranger) > 5:
+        return 'Error: Too many problems.'
+
+    for problem in arranger :
         num1 = problem.split()[0]
         num2 = problem.split()[2]
         operator = problem.split()[1]
-        try:
-            if operator == '+':
-                sum1_2 = int(num1)+int(num2)
-            else:
-                sum1_2 = int(num1)-int(num2)
-        except:
-            return "Error: Numbers must only contain digits."
+
+
         if operator != '+' and operator != '-':
              return "Error: Operator must be '+' or '-'."
-
-        if int(num1) // (10**4) > 0 or int(num2) // (10**4)>0 :
+        if not num1.isdigit() or not num2.isdigit() :
+             return "Error: Numbers must only contain digits."
+        if int(num1) // (10**4)>0 or int(num2) // (10**4)>0 :
              return "Error: Numbers cannot be more than four digits."
-        if len(num1)>len(num2):
-            num1=num1.rjust(len(num1)+2)
-            num2=num2.rjust(len(num1)-1)
-            sum1_2=str(sum1_2).rjust(len(num1))
-            dash='-'*(len(num1))
+        if operator == '+':
+             sum=int(num1) + int(num2)
         else:
-            num1=num1.rjust(len(num2)+2)
-            num2=num2.rjust(len(num2)+1)
-            sum1_2=str(sum1_2).rjust(len(num2)+1)
-            dash='-'*(len(num2)+1)
-        line1.append(num1)
-        line2.append((operator+num2))
-        line3.append(dash)
-        line4.append(sum1_2)
-    line1 = '    '.join(line1)
-    line2 = '    '.join(line2)
-    line3 = '    '.join(line3)
-    line4 = '    '.join(line4)
-    arranged_problems = line1+ '\n' + line2+ '\n' + line3
+             sum=int(num1) - int(num2)
+        if len(num1)> len(num2):
+             num1 = num1.rjust(len(num1)+2)
+             num2 = num2.rjust(len(num1)-1)
+        else:
+             num1 = num1.rjust(len(num2)+2)
+             num2 = num2.rjust(len(num2)+1)
+        sum = str(sum).rjust(len(num1))
+        nums1.append(num1)
+        operators_nums2.append(operator+num2)
+        dash.append('-'*len(num1))
+        sums.append(sum)
+
+    nums1= '    '.join(nums1)
+    operators_nums2 = '    '.join(operators_nums2)
+    dashs = '    '.join(dash)
+    sums = '    '.join(sums)
     if answer == True :
-        arranged_problems += '\n'+line4
-    return arranged_problems
-
-
-
-
-
-
-
-arguments = []
-print(arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43', '123 + 49', '988 + 40'], True))
+        return nums1 +"\n" +operators_nums2+'\n' +dashs+'\n' +sums
+    else:
+        return nums1+'\n' +operators_nums2+'\n'+ dashs
